9795d99f9776cd9f09427ed79114e3a19375452c
Max Wällstedt <max.wallstedt@foreseeti.com>
2019-09-27 16:22:16 +0200

    Update links

--- src/views/home.js
+++ src/views/home.js
@@ -4,7 +4,6 @@ export default () => {
   return (
     <div>
       <h1>Welcome to Meta Attack Language</h1>
-      <img src="https://www.zone-films.stream/uploads/posts/2018-08/1534072218_snkqksf.jpg" />
     </div>
   );
 }
--- src/views/references.js
+++ src/views/references.js
@@ -2,12 +2,16 @@ import React from "react";
 
 const links = [
   {
-    link: 'https://www.foreseeti.com',
-    name: 'foreseeti homepage',
+    link: 'https://github.com/mal-lang/mal-documentation/wiki',
+    name: 'Wiki',
   },
   {
-    link: 'https://www.foreseeti.com',
-    name: 'MAL repository',
+    link: 'https://github.com/mal-lang/malcompiler',
+    name: 'malcompiler repo',
+  },
+  {
+    link: 'https://github.com/mal-lang/exampleLang',
+    name: 'exampleLang repo',
   }
 ]
 
@@ -16,7 +20,6 @@ const LinkItem = ({ name, link }) => (
     <span>
       {`${name}: `}
       <a href={link} target="_blank" rel="noopener noreferrer">{link}</a>
-      }
     </span>
   </li>
 );
